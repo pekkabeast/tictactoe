0ae64f9ec78b21bf1af1fb7b154c85cd0189cec9
Victor Chan <vctrchn@hotmail.ca>
2023-01-15 12:35:43 -0500

    main functionality is working to play tic tac toe with local players

--- script.js
+++ script.js
@@ -7,27 +7,31 @@
 const game = (() => {
   let turn = 0;
 
-  //Function to check if any player won
-  const checkWin = (arrBoard) => {
+  //Helper function to check if any player won
+  const __checkWin = (arrBoard) => {
+    //Check columns
     for (let i = 0; i < 3; i++) {
-      let marker = document.querySelector(`#${i}`);
-      if (marker.textContent != null) {
+      let marker = document.getElementById(`${i}`);
+      if (marker.textContent != null && marker.textContent != "") {
         marker = marker.textContent;
       } else {
         marker = "blank";
       }
+
       if (
         arrBoard[i] == marker &&
         arrBoard[i + 3] == marker &&
         arrBoard[i + 6] == marker
       ) {
+        console.log(1);
         return true;
       }
     }
 
+    //Check rows
     for (let i = 0; i < 7; i += 3) {
-      let marker = document.querySelector(`#${i}`);
-      if (marker.textContent != null) {
+      let marker = document.getElementById(`${i}`);
+      if (marker.textContent != null && marker.textContent != "") {
         marker = marker.textContent;
       } else {
         marker = "blank";
@@ -37,18 +41,19 @@ const game = (() => {
         arrBoard[i + 1] == marker &&
         arrBoard[i + 2] == marker
       ) {
+        console.log(2);
         return true;
       }
     }
 
-    let marker0 = document.querySelector("#0");
-    if (marker0.textContent != null) {
+    let marker0 = document.getElementById("0");
+    if (marker0.textContent != null && marker0.textContent != "") {
       marker0 = marker0.textContent;
     } else {
       marker0 = "blank";
     }
-    let marker2 = document.querySelector("#2");
-    if (marker2.textContent != null) {
+    let marker2 = document.getElementById("2");
+    if (marker2.textContent != null && marker2.textContent != "") {
       marker2 = marker2.textContent;
     } else {
       marker2 = "blank";
@@ -58,6 +63,7 @@ const game = (() => {
       arrBoard[4] == marker0 &&
       arrBoard[8] == marker0
     ) {
+      console.log(3);
       return true;
     }
     if (
@@ -65,27 +71,63 @@ const game = (() => {
       arrBoard[4] == marker2 &&
       arrBoard[6] == marker2
     ) {
+      console.log(4);
       return true;
     }
 
     return false;
   };
 
-  //function to start game
+  //Function to start game
   //Should create two player instances
   //Should initialize arrBoard and display
   const startGame = () => {
     //1. Initialize player instances with marker X and marker O.
     const player1 = player("X");
     const player2 = player("O");
-    const player = [player1, player2];
+    const playerArr = [player1, player2];
     //2. Initialize an empty game Board -> fill arrBoard with empty strings and update display.
     gameBoard.initBoard();
-    //3. Play game where players take turns clicking grids which will update that grid with their respective marker
-    while (!checkWin(gameBoard.getBoard()) || turn > 9) {
-      player.makeMove();
-    }
+
+    //3. Start playing game
+    __playGame(playerArr);
   };
+
+  //function to play game
+  const __playGame = (playerArr) => {
+    //Helper function when a player clicks a grid will change the textContent and then disable click on the same grid after.
+    const handler = (event) => {
+      if (turn % 2 == 0) {
+        makeMove(event, playerArr[0]);
+      } else {
+        makeMove(event, playerArr[1]);
+      }
+    };
+
+    const makeMove = (event, player) => {
+      console.log(event.target);
+      turn += 1;
+      event.target.textContent = player.marker;
+      console.log(event.target.id);
+      gameBoard.setBoard(event.target.id, player);
+
+      event.target.removeEventListener("click", handler);
+      if (__checkWin(gameBoard.getBoard())) {
+        __endGame(player.name);
+      }
+    };
+
+    //Add event listeners to each grid.
+    const grids = document.querySelectorAll(".gridSquare");
+    grids.forEach((button) => button.addEventListener("click", handler));
+  };
+
+  const __endGame = () => {
+    const endOverlay = document.querySelector(".end-overlay");
+    endOverlay.style.display = "flex";
+    turn = 0;
+  };
+  return { startGame };
 })();
 
 //GameBoard Module
@@ -103,7 +145,7 @@ const gameBoard = (() => {
 
   //setter for arrBoard
   const setBoard = (index, currPlayer) => {
-    arrBoard[index] = currPlayer.getMarker();
+    arrBoard[index] = currPlayer.marker;
   };
 
   return {
@@ -117,12 +159,24 @@ const gameBoard = (() => {
 const displayControl = (() => {
   //Function to initialize display
   const init = () => {
+    const removeOverlay = () => {
+      const overlay = document.querySelector(".blur-overlay");
+      overlay.classList.add("no-overlay");
+    };
+
+    const removeEndOverlay = () => {
+      const overlay = document.querySelector(".end-overlay");
+      overlay.style.display = "none";
+    };
+    //Allow start button to reset grid and start game
     const startBtn = document.getElementById("start-btn");
     startBtn.addEventListener("click", () => game.startGame());
-    const grids = document.querySelectorAll(".gridSquare");
-    grids.forEach((button) =>
-      button.addEventListener("click", game.makeMove())
-    );
+    startBtn.addEventListener("click", removeOverlay);
+
+    //Allow restart button to reset grid and play again
+    const restartBtn = document.getElementById("restart-btn");
+    restartBtn.addEventListener("click", () => game.startGame());
+    restartBtn.addEventListener("click", removeEndOverlay);
   };
 
   //function to get the most up to date gameBoard array and then update display with the values
@@ -141,8 +195,6 @@ displayControl.init();
 
 //Player factory function
 //Should contain method to click on gameboard and place marker
-const player = (marker) => {
-  const getMarker = () => marker;
-
-  return { getMarker };
+const player = (marker, name) => {
+  return { marker, name };
 };
